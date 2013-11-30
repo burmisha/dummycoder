d2484f2a5ff9f68de54cd107e879f8fcab78e603
Mikhail Burmistrov <i.like.spam@yandex.ru>
2013-11-30 16:49:56 +0400

    get posted data

--- hello.py
+++ hello.py
@@ -9,5 +9,6 @@ def hello():
 	if request.method == 'GET':
 		return render_template('hello.html')
 	if request.method == 'POST':
-		return render_template('layout.html')
+		q = request.form['query']
+		return render_template('layout.html', query=q)
 	return "Hello!"
