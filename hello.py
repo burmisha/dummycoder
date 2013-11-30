ae9a1d8d12da2a3ddd3c44b61dcf5a4cd0b34974
Mikhail Burmistrov <i.like.spam@yandex.ru>
2013-11-30 22:19:02 +0400

    more code

--- hello.py
+++ hello.py
@@ -4,31 +4,43 @@ import requests
 import json
 import re
 import sys
+from urllib import urlencode
+from collections import OrderedDict
 
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-	token = request.cookies.get("access_token")
+	# token = request.cookies.get("access_token")
+	token = "97ae087c18351e1b53a41a5f3779413bd020f32c"
+	q=""
 	if request.method == 'GET':
 		return render_template('hello.html')
 	if request.method == 'POST':
-		q = request.form['query']
-		r = requests.get('https://api.github.com/search/issues?access_token=' + token + '&q=' + q)
+		p = OrderedDict()
+		p["q"] = request.form["query"]
+		p["language"] = request.form["language"]
+		p["access_token"] = token
+		r = requests.get('https://api.github.com/search/issues', params=urlencode(p))
 		j = r.json()
-		items=[]
+		# q = j
+		issues=[]
 		i = 1
 		for item in j["items"]:
 			i = i + 1;
-			if i < 3:
+			if i < 4:
 				cut_prefix = re.sub("^https://github.com/", "", item["html_url"])
 				[user, repo_url, _] = cut_prefix.split("/", 2)
 				repo = requests.get('https://api.github.com/repos/' + user + "/" + repo_url + '?access_token=' + token).json()
-				items.append(dict({'description': repo["description"], "html_url": item["html_url"]}))
+				issue = dict()
+				issue["description"] = repo["description"]
+				issue["html_url"] = item["html_url"]
+				issue["homepage"] = repo["homepage"]
+				issues.append(issue)
 			else: 
-				items.append(dict({'description': "sample_desc", "html_url": item["html_url"]}))
-		return render_template('layout.html', items=j["items"])
+				issues.append(dict({'description': "sample_desc", "html_url": item["html_url"]}))
+		return render_template('layout.html', items=issues, q=q)
 	return "Hello!"
 
 
