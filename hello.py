b536f62b2cec1a1240696ca1631726519111be3d
Mikhail Burmistrov <i.like.spam@yandex.ru>
2013-11-30 19:49:37 +0400

    get repo info from github

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 import requests
 import json
+import re
 
 app = Flask(__name__)
 app.debug = True
@@ -16,5 +17,12 @@ def hello():
 		r = requests.get('https://api.github.com/search/issues?q=' + q)
 		j = r.json()
 		# urls = [ a["url"] for a in j["items"] ]
+		items=[]
+		for item in j["items"]:
+			cut_prefix = re.sub("^https://github.com/", "", item["html_url"])
+			[user, repo_url, _] = cut_prefix.split("/", 2)
+			repo = requests.get('https://api.github.com/repos/' + user + "/" + repo_url).json()
+			# return 
+			items.append(dict({'description': repo["description"], "html_url": item["html_url"]}))
 		return render_template('layout.html', items=j["items"])
 	return "Hello!"
